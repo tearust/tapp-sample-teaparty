d94604304662e0f1ce572fcdee5f5222ab4d7c65
jacky.li <jacky.li@accenture.com>
2022-01-23 11:04:34 +0800

    update fe version

--- party-fe/shell/stage.sh
+++ party-fe/shell/stage.sh
@@ -3,4 +3,4 @@ echo 'build for stage'
 npm run testnet
 
 echo 'push to 134.209.69.224'
-ipfs add -r dist --api /ip4/134.209.69.224/tcp/5001/p2p/12D3KooWHQjfo3NRhdbcpn8dVdDrPfrHuTYrzrWmBcgxVy8hmFrJ
+ipfs add -r dist --api /ip4/159.89.149.143/tcp/5001/p2p/12D3KooWGDQ9hb5YqcfFaWEJJVNrs9eii5hgvahTAAbz6iuVZsjH
