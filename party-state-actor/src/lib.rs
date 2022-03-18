0711cb8ced36efed0f7ede36373a5435bca4cd2e
kevingzhang <kevin.zhang.canada@gmail.com>
2022-03-18 04:53:12 +0000

    add comments

--- party-state-actor/src/lib.rs
+++ party-state-actor/src/lib.rs
@@ -67,17 +67,25 @@ fn handle_system_init() -> anyhow::Result<()> {
 	Ok(())
 }
 
+/// Get the state's last executed txn's tsid.
+/// The next to-be-executed txn will be based on this tsid
+/// This tsid is also used to determine if the state is out of date
+/// If all state machine replica up to date, this tsid 
+/// shoudl be consistent. 
 fn helper_get_state_tsid() -> HandlerResult<Tsid> {
 	let tsid_bytes: Vec<u8> = actor_statemachine::query_state_tsid()?;
 	let tsid: Tsid = bincode::deserialize(&tsid_bytes)?;
 	Ok(tsid)
 }
 
+/// this is the entrance of all txn (command) execution.
 fn handle_txn_exec(msg: BrokerMessage) -> HandlerResult<()> {
-	
+	// 
 	let (tsid, txn_bytes): (Tsid, Vec<u8>) = bincode::deserialize(&msg.body)?;
+	// Call the txn_exec_inner in order to catch the Err.
 	if let Err(e) = txn_exec_inner(tsid, &txn_bytes) {
 		let txn_hash = tsid.hash;
+		//report_txn_error can send back the error message to the caller (in hosting CML)
 		report_txn_error(txn_hash.clone().to_vec(), e.to_string())?;
 		return Err(format!(
 			"party-state-actor handle txn exec err. hash: 0x{}) exec error: {}",
@@ -89,7 +97,9 @@ fn handle_txn_exec(msg: BrokerMessage) -> HandlerResult<()> {
 	Ok(())
 }
 
-// calculate fee with msg ttl.
+/// calculate fee with msg ttl.
+/// This is the billing model. 
+/// If you want to change the billing mode, this is the place to change
 fn calculate_fee(ttl: u32) -> Balance {
 	let amt: Balance = if ttl > 40000 {
 		3000000000000 as Balance
@@ -103,6 +113,9 @@ fn calculate_fee(ttl: u32) -> Balance {
 	amt
 }
 
+/// This is the actualy command (txn) execution function entry
+/// This function (inside the match clause) is where you business logic
+/// located in the code
 fn txn_exec_inner(tsid: Tsid, txn_bytes: &[u8]) -> HandlerResult<()> {
 	// info!("before TeapartyTxn der");
 	let sample_txn: TeapartyTxn = bincode::deserialize(txn_bytes)?;
@@ -110,6 +123,7 @@ fn txn_exec_inner(tsid: Tsid, txn_bytes: &[u8]) -> HandlerResult<()> {
 	let base: Tsid = helper_get_state_tsid()?;
 	// info!("base tsid is {:?}", &base);
 	let (context_bytes, auth_key): (Vec<u8>, AuthKey) = match sample_txn {
+		/// PostMessage, when user post a new message
 		TeapartyTxn::PostMessage {
 			token_id,
 			from,
@@ -117,9 +131,7 @@ fn txn_exec_inner(tsid: Tsid, txn_bytes: &[u8]) -> HandlerResult<()> {
 			auth_b64,
 		} => {
 			info!("PostMessage => from ttl: {:?},{:?}", &from, &ttl);
-
 			let amt = calculate_fee(ttl);
-
 			let auth_key: AuthKey = bincode::deserialize(&base64::decode(auth_b64)?)?;
 			let auth_ops_bytes = actor_statemachine::query_auth_ops_bytes(auth_key)?;
 			let ctx = TokenContext::new(tsid, base, token_id, &auth_ops_bytes)?;
@@ -131,6 +143,9 @@ fn txn_exec_inner(tsid: Tsid, txn_bytes: &[u8]) -> HandlerResult<()> {
 			(actor_statemachine::consume_from_account(req)?, auth_key)
 		}
 
+		/// User click the extend message button to extend
+		/// the life span of existing message
+		/// of course he will need to pay
 		TeapartyTxn::ExtendMessage {
 			token_id,
 			from,
@@ -172,6 +187,7 @@ fn txn_exec_inner(tsid: Tsid, txn_bytes: &[u8]) -> HandlerResult<()> {
 			(actor_statemachine::consume_from_account(req)?, auth_key)
 		}
 
+		/// user send a private message with notification feature
 		TeapartyTxn::AddNotificationMessage {
 			token_id,
 			from,
