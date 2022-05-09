0ba0b9a942dc9e0bfc44dc2ca34f60174ffc40ae
jacky.li <jacky.li@accenture.com>
2022-05-09 16:20:47 +0800

    bump

--- party-fe/shell/prod.sh
+++ party-fe/shell/prod.sh
@@ -6,3 +6,4 @@ npm run build
 echo 'push to ipfs bootnode [/ip4/64.227.49.206/tcp/5001/p2p/12D3KooWScg336x2Rzc97ZnHbYAEd592P3DqkYJFZRQneGopjsyT]'
 ipfs add -r dist --api /ip4/64.227.49.206/tcp/5001/p2p/12D3KooWScg336x2Rzc97ZnHbYAEd592P3DqkYJFZRQneGopjsyT
 
+# ipfs add -r dist --api /ip4/127.0.0.1/tcp/5001/p2p/12D3KooWCtQAJUyXvWfEThS8gxnvCU8iv2hFHWXq32fwLwqWCrbT
